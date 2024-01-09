354509201746b4cd659fe6fdbb6e1e82292add5a
huangshuqi <1105374939@qq.com>
2024-01-09 14:09:26 +0800

    Upload Clip models

---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+from .resnet_clip import resnet50_clip, resnet50_16_clip, resnet50_64_clip, resnet101_clip, resnet50_4_clip
+from .resnet_clip_load import *
+from .vit_clip import vit_base_patch16_224_clip_laion2b, vit_large_patch14_224_clip_laion2b, \
+    vit_base_patch16_224_augreg_in1k, vit_base_patch16_224_augreg_in21k_ft_in1k, \
+    vit_small_patch16_224_augreg_in1k, vit_small_patch16_224_augreg_in21k_ft_in1k, \
+    vit_large_patch16_224_augreg_in21k_ft_in1k
